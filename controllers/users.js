1ac2afbdabe6c899b705f7252f82257179b9951b
Tuong <lengoctuong23052002@gmail.com>
2022-07-16 20:06:55 +0700

    [01_setup-connectToMongoDB]

---
+++ controllers/users.js
@@ -0,0 +1,7 @@
+const User = require(('../model/model'))
+
+const userController = {
+    
+}
+
+module.exports = userController
---
+++ routes/users.js
@@ -0,0 +1,6 @@
+const userController = require('../controllers/users')
+const express = require('express')
+
+const router = express.Router()
+
+module.exports = router

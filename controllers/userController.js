5e2700fa4f0012fd1ccad7572e93145ba2a3b109
Tuong <lengoctuong23052002@gmail.com>
2022-07-16 20:56:31 +0700

    [03_getUser-findById()]

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,7 +9,16 @@ const userController = {
         } catch(err) {
             res.status(500).json(err)
         }
-    }
+    },
+    getUser: async(req, res) => {
+        try {
+            const user = await User.findById(req.params.id)
+
+            res.status(200).json(user)
+        } catch(err) {
+            res.status(500).json(err)
+        }
+    },
 }
 
 module.exports = userController
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', userController.getUsers)
+router.get('/:id', userController.getUser)
 
 module.exports = router
